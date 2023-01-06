4ce4768f4d6cdbd9757120931f67102fafc9f86d
leomuko <kezzieleo@gmail.com>
2023-01-06 17:13:29 +0300

    Set up initial Routes

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+
+//middle ware 
+
+//since we are getting json
+app.use(express.json())
+
+//routes
+app.get('/hello', (req, res) =>{
+    res.send('Task manager app')
+})
+
+
+app.use('/api/v1/tasks', tasks)
+
+
+const port = 3000;
+
+
+app.listen(port, console.log(`server is listening on port ${port} ...`))
