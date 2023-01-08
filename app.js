876aa8de9f61f0442c3a97d6bc6d18fe860a2419
leomuko <kezzieleo@gmail.com>
2023-01-08 17:55:52 +0300

    Handle Response for non existing routes

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require("./middleware/not-found")
 
 //middle ware 
+
+//serve static files
+app.use(express.static('./public'))
 
 //since we are getting json
 app.use(express.json())
 
-//routes
-app.get('/hello', (req, res) =>{
-    res.send('Task manager app')
-})
 
+
+//routes
 
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
 
 
 const port = 3000;
