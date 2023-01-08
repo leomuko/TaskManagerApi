778bad3981b9f9e1152b8638daa0d754a97336da
leomuko <kezzieleo@gmail.com>
2023-01-08 16:24:04 +0300

    Implement Update of Tasks Api Functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -33,8 +33,22 @@ const getTask =  async (req, res) =>{
     
 }
 
-const updateTask = (req, res) =>{
-    res.send('update task')
+const updateTask = async (req, res) =>{
+    try{
+        const {id: taskId} = req.params
+        const task = await Task.findOneAndUpdate({_id : taskId}, req.body, {
+            new : true,
+            runValidators : true,
+        })
+
+        if(!task){
+            return res.status(404).json({msg : `No task with id : ${taskId}`})
+        }
+        res.status(200).json({task})
+
+    }catch(error){
+        res.status(500).json({msg : error})
+    }
 }
 
 const deleteTask = async (req, res) =>{
