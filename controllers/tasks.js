7dff4b423c33ac6a49c0b79eacb0eec162cb6591
leomuko <kezzieleo@gmail.com>
2023-01-08 15:46:42 +0300

    Implement Fetching single Task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -19,8 +19,18 @@ const createTask = async (req, res) =>{
     
 }
 
-const getTask = (req, res) =>{
-    res.json({id : req.params.id})
+const getTask =  async (req, res) =>{
+    try{
+        const {id : taskId} = req.params
+        const task = await Task.findOne({ _id : taskId})
+        if(!task){
+            return res.status(404).json({msg : `No task with id ${taskId}`})
+        }
+        res.status(200).json({task})
+    }catch(error){
+        res.status(500).json({msg : error})
+    }
+    
 }
 
 const updateTask = (req, res) =>{
